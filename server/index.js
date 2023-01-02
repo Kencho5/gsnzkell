5b8962826699b156f43ed8dada830a19a23243f2
Kencho5 <giokenchadze@gmail.com>
2023-01-02 17:58:32 +0400

    test

--- server/index.js
+++ server/index.js
@@ -587,4 +587,4 @@ app.post('/api/home', (req, res) => {
   });
 });
 
-app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
+app.listen(3000, () => console.log(`Started server at http://localhost:3000!`));
