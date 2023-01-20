dd847f73f0104151c9cbf644d0bbb51ca4203219
Kencho5 <giokenchadze@gmail.com>
2023-01-20 21:50:37 +0400

    added upload form on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HomeService } from './home.service';
-
+import { UploadService } from '../upload/upload.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,23 @@ import { HomeService } from './home.service';
 })
 export class HomeComponent implements OnInit {
   posts = [];
+  urls = [];
+  message: boolean;
+  form_msg: boolean;
+
+    uploadForm = new FormGroup({
+    animal:  new FormControl('', Validators.required),
+    breed:  new FormControl('', Validators.required),
+    price:  new FormControl(''),
+    age:  new FormControl('', Validators.required),
+    ageType:  new FormControl('', Validators.required),
+    description:  new FormControl('', Validators.maxLength(200)),
+    postType:  new FormControl('', Validators.required),
+    phone:  new FormControl('', Validators.required),
+    imgs:  new FormControl('', Validators.required),
+    city:  new FormControl('', Validators.required)
+  });
+
 
   searchForm = this.formBuilder.group({
     text:  new FormControl()
@@ -18,7 +35,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private homeService: HomeService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private uploadService: UploadService
   ) { }
 
   ngOnInit(): void {
@@ -54,5 +72,60 @@ export class HomeComponent implements OnInit {
 
     div.scrollLeft += div.offsetWidth;
   }
+
+  changeInput(event) {
+    if(event.target.value != "Selling") {
+      event.path[3][3].style.display = 'none';
+    } else {
+      event.path[3][3].style.display = 'block';
+    }
+  }
+
+    selectFiles(event) {
+    if(event.target.files) {
+      if(event.target.files.length > 3 || this.urls.length > 3) {
+        this.message = true;
+        return;
+      }
+      this.message = false;
+      for(var i = 0; i <= File.length; i++) {
+        var reader = new FileReader();
+
+        reader.readAsDataURL(event.target.files[i]);
+        reader.onload = (event: any) => {
+          this.urls.push(event.target.result)
+        }
+      }
+    }
+  }
+
+    removeImage(event) {
+    var tmp = [];
+    this.urls.forEach(url => {
+      if(url != event.target.classList[2]) {
+        tmp.push(url)
+      }
+    });
+    this.urls = tmp;
+  }
+
+    upload() {
+    if(this.uploadForm.valid) {
+      const data = {
+        user: localStorage.getItem('token'),
+        form: this.uploadForm.value,
+        urls: this.urls
+      }
+
+      this.uploadService.uploadPost(data).subscribe((res) => {
+        if (res["code"] == 200) {
+          this.router.navigate(['/post', res['id']]);
+        }
+      });
+    } else {
+      this.form_msg = true;
+    }
+  }
+
 
 }
