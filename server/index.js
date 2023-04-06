1607319e957d36eb4d3f8f7824fbf4779d1b11f3
Kencho5 <giokenchadze@gmail.com>
2023-04-06 21:00:42 +0400

    Update server paths for saving images and user profile pictures

--- server/index.js
+++ server/index.js
@@ -276,7 +276,7 @@ app.post("/api/update", (req, res) => {
 function savePfp(pfp, id) {
   const savePath = os.platform() === "darwin"
     ? "../src/assets/images"
-    : "/root/";
+    : "/usr/share/nginx/pender/assets/images";
 
   const base64Data = pfp;
 
@@ -421,7 +421,7 @@ app.post("/api/upload", async (req, res) => {
 async function saveImages(postID, req) {
   const savePath = os.platform() === "darwin"
     ? "../src/assets/"
-    : "/root/";
+    : "/usr/share/nginx/pender/assets/";
   await fs.promises.mkdir(`${savePath}/postImages/${postID}`);
 
   const imgs = [];
