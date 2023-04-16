59f6914b4960fd4f31613a620ae8ba022d43ebec
Kencho5 <giokenchadze@gmail.com>
2023-04-16 15:07:23 +0400

    fixed latest posts sometimes not loading on home page

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,67 +1,55 @@
 const { db, userPosts, users } = require("../utils/db");
 
 async function home(req, res) {
-  var posts = [];
-  var vipPosts = [];
+  try {
+    const posts = await userPosts
+      .find()
+      .limit(8)
+      .sort({ $natural: -1 })
+      .toArray();
 
-  var result = await userPosts
-    .find()
-    .limit(8)
-    .sort({
-      $natural: -1,
-    })
-    .toArray(function (err, results) {
-      if (results) {
-        results.forEach((result) => {
-          posts.push({
-            id: result._id,
-            email: result.email,
-            animal: result.animal,
-            breed: result.breed,
-            price: result.price,
-            age: result.age,
-            ageType: result.ageType,
-            postType: result.postType,
-            date: result.date,
-            imgs: result.img_path,
-            vip: result.vip,
-          });
-        });
-      }
-    });
+    const vipPosts = await userPosts
+      .find({ vip: true })
+      .limit(4)
+      .sort({ expires: -1 })
+      .toArray();
 
-  var vipResult = await userPosts
-    .find({
-      vip: true,
-    })
-    .limit(4)
-    .sort({
-      expires: -1,
-    })
-    .toArray(function (err, results) {
-      if (results) {
-        results.forEach((result) => {
-          vipPosts.push({
-            id: result._id,
-            email: result.email,
-            animal: result.animal,
-            breed: result.breed,
-            price: result.price,
-            age: result.age,
-            ageType: result.ageType,
-            postType: result.postType,
-            date: result.date,
-            imgs: result.img_path,
-            vip: result.vip,
-          });
-        });
-      }
-      res.status(200).send({
-        code: 200,
-        posts: posts,
-        vipPosts: vipPosts,
-      });
+    const formattedPosts = posts.map(post => ({
+      id: post._id,
+      email: post.email,
+      animal: post.animal,
+      breed: post.breed,
+      price: post.price,
+      age: post.age,
+      ageType: post.ageType,
+      postType: post.postType,
+      date: post.date,
+      imgs: post.img_path,
+      vip: post.vip,
+    }));
+
+    const formattedVipPosts = vipPosts.map(post => ({
+      id: post._id,
+      email: post.email,
+      animal: post.animal,
+      breed: post.breed,
+      price: post.price,
+      age: post.age,
+      ageType: post.ageType,
+      postType: post.postType,
+      date: post.date,
+      imgs: post.img_path,
+      vip: post.vip,
+    }));
+
+    res.status(200).send({
+      code: 200,
+      posts: formattedPosts,
+      vipPosts: formattedVipPosts,
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ code: 500, message: 'Internal server error' });
+  }
 }
-
 module.exports = home;
