e61eb520a16005cfaa0d0946d03fde0cd01602a2
Kencho5 <giokenchadze@gmail.com>
2023-04-16 15:34:15 +0400

    Refactor counter component and remove unnecessary isLoading flag

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-var isLoading = true;
-window.addEventListener('load', () => {
-  isLoading = false;
-});
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
