14805e19173e65e374c7d1c33d583eae5763da59
Kencho5 <giokenchadze@gmail.com>
2023-04-16 15:30:39 +0400

    Add *ngIf directive to counter div to show only when isLoading is true

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+var isLoading = true;
+window.addEventListener('load', () => {
+  isLoading = false;
+});
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
