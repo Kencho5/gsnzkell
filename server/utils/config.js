a2f976a60a847c16d84ebe4774df8db7619d7b89
Kencho5 <giokenchadze@gmail.com>
2023-04-28 00:28:54 +0400

    Update region in server utils config.js from eu-north-1 to eu-central-1

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "eu-north-1",
+  region: "eu-central-1",
 };
