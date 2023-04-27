7a665caef130d01930986d592339eb38fbe38fcb
Kencho5 <giokenchadze@gmail.com>
2023-04-27 16:00:52 +0400

    test

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,10 @@ import { Component, AfterViewInit } from '@angular/core';
   styleUrls: ['./footer.component.scss', '../../responsive.css'],
 })
 export class FooterComponent {
-  ngOnInit() {
-    const script = document.createElement('script');
-    script.src = '//counter.top.ge/counter.js';
-    script.async = true;
-    document.body.appendChild(script);
-  }
+  // ngOnInit() {
+  //   const script = document.createElement('script');
+  //   script.src = '//counter.top.ge/counter.js';
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  // }
 }
