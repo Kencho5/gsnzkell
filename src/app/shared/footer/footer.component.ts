78f22908a07e33992b24371163c8730732fa4856
Kencho5 <giokenchadze@gmail.com>
2023-04-27 12:35:58 +0400

    Add Top.ge async counter code to footer component and remove it from index.html

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss', '../../responsive.css']
+  styleUrls: ['./footer.component.scss', '../../responsive.css'],
 })
 export class FooterComponent {
-
+  ngAfterViewInit() {
+    const script = document.createElement('script');
+    script.src = '//counter.top.ge/counter.js';
+    document.body.appendChild(script);
+  }
 }
