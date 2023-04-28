b63ba509b2b4e7d46e5c67d41becc664b09cfa07
Kencho5 <giokenchadze@gmail.com>
2023-04-28 11:39:39 +0400

    Add TBC bank API access token and API key to config file for TBC payment processing

--- server/controllers/auth/tbcPayment.js
+++ server/controllers/auth/tbcPayment.js
@@ -1,9 +1,15 @@
 const { db, userPosts, users } = require("../../utils/db");
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+const config = require("../../utils/config");
 
 async function payment(req, res) {
   const amount = req.body.amount;
   const user = req.body.user;
 
+  const apiUrl = 'https://api.tbcbank.ge/v1/tpay/payments';
+  const accessToken = config.tbcAccessToken;
+  const apiKey = config.tbcApiKey;
 
   return res.status(200).send({
     code: 200,
--- server/utils/config.js
+++ server/utils/config.js
@@ -4,4 +4,6 @@ module.exports = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "eu-central-1",
+  tbcAccessToken: process.env.TBC_ACCESS_TOKEN,
+  tbcApiKey: process.env.TBC_API_KEY
 };
