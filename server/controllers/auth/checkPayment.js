c7e54b35951a3edc5af1cd9ecbe7ff13e85d311f
Kencho5 <giokenchadze@gmail.com>
2023-04-28 12:00:40 +0400

    Add error message display to payment form in balance component's HTML file

--- server/controllers/auth/checkPayment.js
+++ server/controllers/auth/checkPayment.js
@@ -5,6 +5,7 @@ const accessToken = config.tbcAccessToken;
 const apiKey = config.tbcApiKey;
 
 async function checkPayment(req, res) {
+  console.log(req.body)
   const apiUrl = "https://api.tbcbank.ge/v1/tpay/payments";
 
   const headers = {
