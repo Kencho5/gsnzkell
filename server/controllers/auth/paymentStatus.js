1ff609ce74ac85a1db1410c2c8bcb2ef1d1834b2
Kencho5 <giokenchadze@gmail.com>
2023-04-28 13:27:33 +0400

    Refactor paymentStatus to improve readability and handle errors

--- server/controllers/auth/paymentStatus.js
+++ server/controllers/auth/paymentStatus.js
@@ -6,39 +6,40 @@ const { privateKEY, publicKEY, signOptions } = require("../../models/token");
 async function paymentStatus(req, res) {
   const email = req.body.email;
 
-  payments.findOne({ merchantPaymentId: req.body.merchantPaymentId }, async (err, payment) => {
-    console.log(payment, req.body.merchantPaymentId, req.body)
-    if (err || payment == null) {
-      return res.status(200).send({
-        code: 404,
-      });
-    } else {
-
-      await users.findOne({ email }, (err, responseDB) => {
-        const payload = {
-          id: responseDB._id,
-          username: responseDB.username,
-          email,
-          phone: responseDB.phone,
-          instagram: responseDB.instagram,
-          facebook: responseDB.facebook,
-          city: responseDB.city,
-          balance: responseDB.balance.toFixed(2),
-          freeUpload: responseDB.freeUpload,
-          pfp: responseDB.pfp,
-        };
-        
-        const balance = responseDB.balance.toFixed(2);
-        const token = jwt.sign(payload, privateKEY, signOptions);
-
+  payments.findOne(
+    { merchantId: req.body.merchantPaymentId },
+    async (err, payment) => {
+      if (err || payment == null) {
         return res.status(200).send({
-          code: 200,
-          token: token,
-          balance: balance
+          code: 404,
         });
-      });
+      } else {
+        await users.findOne({ email }, (err, responseDB) => {
+          const payload = {
+            id: responseDB._id,
+            username: responseDB.username,
+            email,
+            phone: responseDB.phone,
+            instagram: responseDB.instagram,
+            facebook: responseDB.facebook,
+            city: responseDB.city,
+            balance: responseDB.balance.toFixed(2),
+            freeUpload: responseDB.freeUpload,
+            pfp: responseDB.pfp,
+          };
+
+          const balance = responseDB.balance.toFixed(2);
+          const token = jwt.sign(payload, privateKEY, signOptions);
+
+          return res.status(200).send({
+            code: 200,
+            token: token,
+            balance: balance,
+          });
+        });
+      }
     }
-  });
+  );
 }
 
 module.exports = paymentStatus;
