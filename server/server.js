d8d24c829abba3e6db2362f6719ff0160910f090
Kencho5 <giokenchadze@gmail.com>
2023-05-01 12:21:13 +0400

    Add parameter limit to express.urlencoded middleware in server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(
 );
 
 app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', parameterLimit: 50000}));
 
 app.use(express.json());
 
