291da63a678d9a3fd4b2b1eea2484c028ae89452
Kencho5 <giokenchadze@gmail.com>
2023-05-02 17:41:41 +0400

    Refactor deletePost function to delete multiple posts and their associated image folders

--- server/controllers/posts/deletePost.js
+++ server/controllers/posts/deletePost.js
@@ -2,17 +2,23 @@ const { db, userPosts, users } = require("../../utils/db");
 const fs = require("fs");
 
 async function deletePost(req, res) {
-  userPosts.deleteMany({
-    _id: { $in: req.body.id },
-  });
+  for (let id of req.body.id) {
+    userPosts.deleteOne({
+      _id: id
+    });
 
-  fs.rmdir(`/var/uploads/postImages/${req.body.id}`, { recursive: true }, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Folder deleted successfully");
-    }
-  });
+    fs.rmdir(
+      `/var/uploads/postImages/${id}`,
+      { recursive: true },
+      (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Folder deleted successfully");
+        }
+      }
+    );
+  }
 
   res.status(200).send({
     code: 200,
