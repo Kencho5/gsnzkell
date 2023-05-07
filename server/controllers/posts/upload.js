29eec545717a1b959f6920220b3c6af2f6a90703
Kencho5 <giokenchadze@gmail.com>
2023-05-07 20:26:27 +0400

    test

--- server/controllers/posts/upload.js
+++ server/controllers/posts/upload.js
@@ -5,11 +5,13 @@ const { v4: uuidv4 } = require("uuid");
 var os = require("os");
 const fs = require("fs");
 const sharp = require("sharp");
+const fetch = require('node-fetch');
 
 async function upload(req, res) {
   const contentLength = parseInt(req.get('content-length'));
   const contentLengthMb = (contentLength / (1024 * 1024)).toFixed(2);
   console.log(`Request size: ${contentLengthMb} MB`);
+  console.log(req.body.urls)
 
   const token = req.body.user;
 
@@ -135,29 +137,25 @@ async function saveImages(postID, req) {
   const savePath = "/var/uploads";
   await fs.promises.mkdir(`${savePath}/postImages/${postID}`);
 
-  const imgs = await Promise.all(
-    req.body.urls.map(async (base64Data, i) => {
-      const matches = base64Data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-
-      if (!matches) {
-        return null;
-      }
-
-      const [, type, data] = matches;
-      const buffer = Buffer.from(data, "base64");
+  const imgs = await Promise.all(req.body.urls.map(async (url, i) => {
+    try {
+      const response = await fetch(url);
+      const buffer = await response.buffer();
 
       // Compress the image using sharp
       const compressedBuffer = await sharp(buffer)
         .jpeg({ quality: 60 })
         .toBuffer();
 
-      await fs.promises.writeFile(
-        `${savePath}/postImages/${postID}/${i}.${type.split("/")[1]}`,
-        compressedBuffer
-      );
-      return `${i}.${type.split("/")[1]}`;
-    })
-  );
+      const extension = response.headers.get('content-type').split('/')[1];
+      const filename = `${i}.${extension}`;
+      await fs.promises.writeFile(`${savePath}/postImages/${postID}/${filename}`, compressedBuffer);
+      return filename;
+    } catch (error) {
+      console.error(`Error saving image ${url}: ${error}`);
+      return null;
+    }
+  }));
 
   return imgs.filter((img) => img !== null);
 }
