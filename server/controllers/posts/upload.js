f604434b99bf0d622ba8b83e05aa6688b175bc9c
Kencho5 <giokenchadze@gmail.com>
2023-05-07 16:06:14 +0400

    test

--- server/controllers/posts/upload.js
+++ server/controllers/posts/upload.js
@@ -7,9 +7,10 @@ const fs = require("fs");
 const sharp = require("sharp");
 
 async function upload(req, res) {
-const contentLength = req.get('content-length');
+  const contentLength = req.get('content-length');
   const sizeInMB = contentLength / (1024 * 1024);
   console.log(`Request size: ${sizeInMB.toFixed(2)} MB`);
+
   const token = req.body.user;
 
   if (!jwt.verify(token, publicKEY, signOptions)) {
@@ -89,7 +90,35 @@ const contentLength = req.get('content-length');
 
   var newToken = jwt.sign(payload, privateKEY, signOptions);
 
-  const imgs = await saveImages(postID, req);
+  const savePath = "/var/uploads";
+  if (!fs.existsSync(`${savePath}/postImages/${postID}`)) {
+    await fs.promises.mkdir(`${savePath}/postImages/${postID}`);
+  }
+
+const imgs = await Promise.all(
+  req.body.urls.map(async (base64Data, i) => {
+    const matches = base64Data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+
+    if (!matches) {
+      return null;
+    }
+
+    const [, type, data] = matches;
+    const buffer = Buffer.from(data, "base64");
+
+    // Compress and resize the image using sharp
+    const compressedBuffer = await sharp(buffer)
+      .resize({ width: 800 })
+      .jpeg({ quality: 60 })
+      .toBuffer();
+
+    await fs.promises.writeFile(
+      `${savePath}/postImages/${postID}/${i}.${type.split("/")[1]}`,
+      compressedBuffer
+    );
+    return `${i}.${type.split("/")[1]}`;
+  })
+);
 
   const data = {
     _id: postID,
@@ -104,7 +133,7 @@ const contentLength = req.get('content-length');
     postType: form.postType,
     phone: form.phone,
     date: new Date(),
-    img_path: imgs,
+    img_path: imgs.filter((img) => img !== null),
     city: form.city,
     vip: vip,
     expires: new Date(Date.now() + 21 * 24 * 60 * 60 * 1000),
@@ -128,36 +157,5 @@ const contentLength = req.get('content-length');
       });
     }
   });
-}
-
-async function saveImages(postID, req) {
-  const savePath = "/var/uploads";
-  await fs.promises.mkdir(`${savePath}/postImages/${postID}`);
-
-  const imgs = await Promise.all(
-    req.body.urls.map(async (base64Data, i) => {
-      const matches = base64Data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-
-      if (!matches) {
-        return null;
-      }
-
-      const [, type, data] = matches;
-      const buffer = Buffer.from(data, "base64");
-
-      // Compress the image using sharp
-      const compressedBuffer = await sharp(buffer)
-        .jpeg({ quality: 60 })
-        .toBuffer();
-
-      await fs.promises.writeFile(
-        `${savePath}/postImages/${postID}/${i}.${type.split("/")[1]}`,
-        compressedBuffer
-      );
-      return `${i}.${type.split("/")[1]}`;
-    })
-  );
-
-  return imgs.filter((img) => img !== null);
 }
 module.exports = upload;
