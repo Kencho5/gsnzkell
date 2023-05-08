afa5acc725c79037b906ece89c22b5d57b2bbdd0
Kencho5 <giokenchadze@gmail.com>
2023-05-08 23:24:11 +0400

    added function to run on noon that replaces tbc access token

---
+++ server/controllers/auth/refreshToken.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const config = require("../../utils/config");
+const cron = require("node-cron");
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config();
+
+cron.schedule("0 0 12 * * *", function () {
+  axios
+    .post(
+      "https://api.tbcbank.ge/v1/tpay/access-token",
+      new URLSearchParams({
+        client_Id: config.tbcClientId,
+        client_secret: config.tbcClientSecret,
+      }),
+      {
+        headers: {
+          apikey: config.tbcApiKey,
+        },
+      }
+    )
+    .then(function (response) {
+      console.log("Refreshed Access Token.");
+
+      const accessToken = response.data.access_token;
+      // Update the access token in the .env file
+      const envConfig = dotenv.parse(fs.readFileSync(".env"));
+      envConfig.TBC_ACCESS_TOKEN = accessToken;
+
+      // Write the updated .env file
+      fs.writeFileSync(".env", "");
+      for (const key in envConfig) {
+        fs.appendFileSync(".env", `${key}=${envConfig[key]}\n`);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
