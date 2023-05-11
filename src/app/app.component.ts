844d6b8c6c2134d291b4e059cdf144a3d891c928
Kencho5 <giokenchadze@gmail.com>
2023-05-11 17:37:05 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,6 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   // filterForm = new FormGroup({
   //   animal: new FormControl(''),
   //   postType: new FormControl('', Validators.required),
