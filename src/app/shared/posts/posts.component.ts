afd5b35c1c36c2f5283e8232899ccc2cfea48290
Kencho5 <giokenchadze@gmail.com>
2023-05-19 17:00:51 +0400

    added pagination animations to everytihng

--- src/app/shared/posts/posts.component.ts
+++ src/app/shared/posts/posts.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class PostsComponent {
   constructor(private router: Router, public translate: TranslateService) {}
--- src/app/shared/vip-posts/vip-posts.component.ts
+++ src/app/shared/vip-posts/vip-posts.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-vip-posts',
   templateUrl: './vip-posts.component.html',
-  styleUrls: ['../posts/posts.component.scss']
+  styleUrls: ['../posts/posts.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class VipPostsComponent {
   constructor(private router: Router, public translate: TranslateService) {}
@@ -15,5 +24,4 @@ export class VipPostsComponent {
   openPost(id) {
     this.router.navigate([`/post/${id}`]);
   }
-
 }
